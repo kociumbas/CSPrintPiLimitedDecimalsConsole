7747b6844d410d503f700ca34a5b7821e73db683
kociumbas <kociumbas@outlook.com>
2016-02-16 01:06:51 +0100

    CSPrintPiLimitedDecimalsConsole

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_2WyswietlPi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = Math.PI;
+            Console.WriteLine("Pi = " + Math.Round(pi, 5));   //or
+            Console.WriteLine("Pi = {0:#.#####}",pi);
+            Console.ReadLine();
+        }
+    }
+}
